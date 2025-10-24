5a0e9a9cef80302ed8c426a32e95396d810745d3
prayudahlah <yudafihan@gmail.com>
2025-10-24 18:53:05 +0700

    Added ping context

--- backend/config/db.go
+++ backend/config/db.go
@@ -4,11 +4,16 @@ import (
 	"database/sql"
 	"log"
 	"fmt"
+	"time"
+	"context"
 
 	_ "github.com/microsoft/go-mssqldb"
 )
 
 func InitDB() *sql.DB {
+	log.SetPrefix("[DB] ")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
 	cfg := LoadConfig()
 
 	connStr := fmt.Sprintf(
@@ -24,6 +29,16 @@ func InitDB() *sql.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Koneksi ke database gagal\n-> ", err)
+	}
+
+	log.Println("Koneksi ke database berhasil")
 
 	return db
 }
