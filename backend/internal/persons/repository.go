2d7547c5f2153ba075cf23fcbf90bdeaf29a4d0d
prayudahlah <yudafihan@gmail.com>
2025-10-24 23:34:52 +0700

    feat(persons-repo): persons repo GetByID

---
+++ backend/internal/persons/repository.go
@@ -0,0 +1,46 @@
+package persons
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type PersonRepo interface {
+	GetByID(id string) (*Person, error)
+	// Create(person Person) error
+}
+
+type personRepo struct {
+	db *sql.DB
+}
+
+func NewRepo(db *sql.DB) PersonRepo {
+	return &personRepo{db: db}
+}
+
+func (r *personRepo) GetByID(id string) (*Person, error) {
+	query := "SELECT p.PersonId, p.PrimaryName, p.BirthYear, p.DeathYear " +
+		"FROM PERSONS p " +
+		"WHERE p.PersonId = ?"
+
+	row := r.db.QueryRow(query, id)
+
+	var p Person
+
+	err := row.Scan(
+		&p.PersonID,
+		&p.PrimaryName,
+		&p.BirthYear,
+		&p.DeathYear
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+// func (r *personRepo) Create(person Person) error {
+//
+// }
