d3237ba32af4dba430eedb4ef9e7205a89616a51
prayudahlah <yudafihan@gmail.com>
2025-10-24 23:54:13 +0700

    feat(routes): persons routing and fiber setup

---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"database/sql"
+	"github.com/prayudahlah/showflix/backend/internal/persons"
+)
+
+func SetupRoutes(app *fiber.App, db *sql.DB) {
+	personHandler := persons.NewHandler(persons.NewService(persons.NewRepo(db)))
+
+	api := app.Group("/api")
+
+	// persons
+	api.Get("/persons/:id", personHandler.GetByID)
+}
