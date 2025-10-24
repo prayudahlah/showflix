d6914819e8bb82c5ff1c3886d97f9188904473c7
prayudahlah <yudafihan@gmail.com>
2025-10-24 23:44:49 +0700

    feat(persons-handler): persons handler GetByID

---
+++ backend/internal/persons/handler.go
@@ -0,0 +1,27 @@
+package persons
+
+import "github.com/gofiber/fiber/v2"
+
+type PersonHandler struct {
+	Service PersonService
+}
+
+func NewHandler(service PersonService) *PersonHandler {
+	return &PersonHandler{Service: service}
+}
+
+func (h *PersonHandler) GetByID(c *fiber.Ctx) error {
+	id := c.Params("person_id")
+
+	person, err := h.Service.GetByID(id)
+
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Movie not found"})
+	}
+
+	return c.JSON(person)
+}
+
+// func (h *PersonHandler) Create(c *fiber.Ctx) error {
+//
+// }
