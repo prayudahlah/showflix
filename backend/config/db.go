1b4f25b721001047a0389b3ffee132db01f07697
prayudahlah <yudafihan@gmail.com>
2025-10-24 16:29:52 +0700

    added db.go

---
+++ backend/config/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"backend/config"
+	"database/sql"
+	"log"
+	"fmt"
+
+	_ "github.com/microsoft/go-mssqldb"
+)
+
+func InitDB() *sql.DB {
+	cfg := &config.Load()
+
+	connStr := fmt.Sprintf(
+		"sqlserver://%s:%s@%s:%s/instance?database=%s",
+		cfg.DBUser,
+		cfg.DBPass,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+	)
+
+	db, err := sql.Open("sqlserver", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
