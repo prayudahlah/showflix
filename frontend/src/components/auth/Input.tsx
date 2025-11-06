5f89573532d80c237e12e28b46e2bc1ec77094a3
prayudahlah <yudafihan@gmail.com>
2025-11-07 00:00:10 +0700

    fix(input): input password empty type

--- frontend/src/components/auth/Input.tsx
+++ frontend/src/components/auth/Input.tsx
@@ -1,8 +1,9 @@
 type InputProps = {
   placeholder: string;
+  type?: string;
 }
 
-function Input({ placeholder }: InputProps) {
+function Input({ placeholder, type }: InputProps) {
   return (
     <>
       <input
@@ -10,6 +11,7 @@ function Input({ placeholder }: InputProps) {
         className="bg-primary2-2/30 w-full py-2 px-4 rounded-lg
                    shadow-[inset_2px_2px_4px_rgba(0,0,0,0.35)] focus:outline-none focus:ring-2 focus:ring-primary2-1
                    transition-all leading-tight"
+        type={type ?? ""}
       />
     </>
   )
