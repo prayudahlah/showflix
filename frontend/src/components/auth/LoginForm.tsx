7c2de67337b7687be2ca390ae41a33c071ffe111
prayudahlah <yudafihan@gmail.com>
2025-11-13 15:40:29 +0700

    feat(form): added form functionality redirection

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -1,21 +1,25 @@
+import { useNavigate } from 'react-router'
 import { useLogin } from '../../hooks/useLogin.ts'
 import Input from './Input.tsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function LoginForm() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
-  const loginMutation = useLogin();
+  const login = useLogin()
+  const navigate = useNavigate()
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    loginMutation.mutate({
-      username: username,
-      password: password,
-    })
+    e.preventDefault()
+    login.mutate({ username, password })
   }
+
+  useEffect(() => {
+    if (login.isSuccess && login.data) {
+      navigate(`/dashboard/${login.data.role}`)
+    }
+  }, [login.isSuccess, login.data])
 
   return (
     <form className='flex flex-col items-center w-full' onSubmit={handleSubmit}>
@@ -24,9 +28,7 @@ function LoginForm() {
           placeholder="Username"
           id="username"
           value={username}
-          onChange={e => {
-            setUsername(e.target.value)
-          }}
+          onChange={e => setUsername(e.target.value)}
         />
 
         <Input
@@ -34,36 +36,32 @@ function LoginForm() {
           type="password"
           id="password"
           value={password}
-          onChange={e => {
-            setPassword(e.target.value)
-          }}
+          onChange={e => setPassword(e.target.value)}
         />
       </div>
 
       <div className='flex flex-row justify-between w-[95%] mt-10 mb-6'>
         <button className="hover-scale border border-primary2-1 py-1 rounded-4xl text-primary3-1 w-[45%] shadow-4xl">
-          <span
-            className="
-              bg-linear-to-r from-primary3-2 to-primary3-1
-              bg-clip-text text-transparent">
+          <span className="bg-linear-to-r from-primary3-2 to-primary3-1 bg-clip-text text-transparent">
             BACK
           </span>
         </button>
 
-        <button type="submit" className="hover-scale py-1 rounded-4xl w-[45%]
-                           bg-linear-to-r from-primary3-2 to-primary3-1 text-white">
-          LOG IN
+        <button
+          disabled={login.isPending}
+          type="submit"
+          className="hover-scale py-1 rounded-4xl w-[45%] bg-linear-to-r from-primary3-2 to-primary3-1 text-white"
+        >
+          {login.isPending ? "Loading..." : "LOG IN"}
         </button>
       </div>
 
-      {loginMutation.isError && (
-        <p className="text-red-500 mt-3">Login gagal. Coba lagi.</p>
+      {login.isError && (
+        <p className="text-red-500 mt-3">Invalid Username or Password</p>
       )}
-
-      {loginMutation.isSuccess && (
-        <p className="text-green-500 mt-3">Login sukses!</p>
-      )}    </form>
+    </form>
   )
 }
 
-export default LoginForm;
+export default LoginForm
+
