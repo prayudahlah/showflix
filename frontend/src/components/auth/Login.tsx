a07be871baaa4ac12cd43ea48cecf9d984fb06a2
prayudahlah <yudafihan@gmail.com>
2025-11-13 09:49:47 +0700

    fix(auth): deleted email form

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -16,12 +16,11 @@ function Login() {
       <p>Please enter your information!</p>
 
       <div className='mt-10 flex flex-col gap-8 w-[95%] items-center'>
-        <Input placeholder="Email" />
         <Input placeholder="Username" />
         <Input placeholder="Password" type="password" />
       </div>
 
-      <div className='flex flex-row justify-between w-[95%] mt-10'>
+      <div className='flex flex-row justify-between w-[95%] mt-10 mb-6'>
         <button className="hover-scale border border-primary2-1 py-1 rounded-4xl text-primary3-1 w-[45%] shadow-4xl">
           <span
             className="
